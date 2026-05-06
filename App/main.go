facbc0594642ef50711b6b22340b221812142006
Lenovo 15 <information@com1software.com>
2026-05-06 15:14:29 -0400

    update

--- App/main.go
+++ App/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "github.com/Com1Software/go-clevelandnews/news"
+	news "github.com/Com1Software/go-clevelandnews"
 )
 
 func main() {
-    items, err := news.GetLatest()
-    if err != nil {
-        log.Fatal(err)
-    }
+	items, err := news.GetLatest()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for i, it := range items {
-        if i >= 10 { // just show first 10
-            break
-        }
-        fmt.Printf("%s\n%s\n\n", it.Title, it.Link)
-    }
+	for i, it := range items {
+		if i >= 10 { // just show first 10
+			break
+		}
+		fmt.Printf("%s\n%s\n\n", it.Title, it.Link)
+	}
 }
