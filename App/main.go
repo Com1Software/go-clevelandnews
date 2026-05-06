a79526b3610126ffa5d8f3c2469fea22a8bf231c
Com1 Software, Inc. <information@com1software.com>
2026-05-06 14:59:44 -0400

    Add main function to fetch latest news articles
    Implement main function to fetch and display latest news.

---
+++ App/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/Com1Software/go-clevelandnews/news"
+)
+
+func main() {
+    items, err := news.GetLatest()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for i, it := range items {
+        if i >= 10 { // just show first 10
+            break
+        }
+        fmt.Printf("%s\n%s\n\n", it.Title, it.Link)
+    }
+}
